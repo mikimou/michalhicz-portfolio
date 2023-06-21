c0ccd46cca5476055677fe44c49031fd6c5520b0
Michal Hicz <michal07.h@gmail.com>
2023-06-21 14:31:20 +0200

    bg light update

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -9,7 +9,7 @@ export const graphicsToggle = writable(true);
 export const fov = writable(9);
 export const scale = writable(0);
 export const mouseReactivity = writable(true);
-export const backLight = writable(2.7);
+export const backLight = writable(2.5);
 export const pixelation = writable(80);
 
 /* Theming */
