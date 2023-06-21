8d28aac2eb274626865aacc41be80b623985b631
Michal Hicz <michal07.h@gmail.com>
2023-06-21 02:05:26 +0200

    update

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -5,7 +5,7 @@ export const mousey = writable(0);
 export const screenWidth = writable(0);
 
 /* Settings */
-export const graphicsToggle = writable(false);
+export const graphicsToggle = writable(true);
 export const fov = writable(9);
 export const scale = writable(0);
 export const theme = writable("");
